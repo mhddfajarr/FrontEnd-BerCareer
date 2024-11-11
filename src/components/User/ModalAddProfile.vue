4bd319e3ab1cfc634c2d318395d17a7c248e932e
Muhammad Fajar <mhddfajar@gmail.com>
2024-11-11 20:53:41 +0700

    feat(Profile): Modal
    Add Modal at Profile until Certificate

---
+++ src/components/User/ModalAddProfile.vue
@@ -0,0 +1,94 @@
+<template>
+     <div v-if="showModal" class="fixed inset-0 bg-gray-900 bg-opacity-50 flex items-center justify-center z-50">
+      <!-- Modal Container -->
+      <div class="bg-white p-6 rounded-lg shadow-lg w-11/12 md:w-2/3">
+        <!-- Modal Header -->
+        <div class="flex justify-between items-center border-b pb-3">
+          <h3 class="text-xl font-semibold text-gray-700">Edit Profile</h3>
+          <button @click="closeModal" class="text-gray-500 hover:text-gray-700">✕</button>
+        </div>
+        
+        <!-- Modal Body (Form) -->
+        <div class="mt-4 max-h-full md:max-h-80 overflow-y-auto p-5">
+          <form @submit.prevent="submitForm">
+            <div class="mb-4">
+              <label for="name" class="block text-gray-700 font-semibold text-left">Name</label>
+              <input
+                type="text"
+                id="name"
+                class="w-full text-gray-700 border bg-white rounded px-3 py-2 mt-1 focus:outline-none focus:ring focus:ring-primary/50"
+                required
+              />
+            </div>
+            <div class="mb-4">
+              <label for="email" class="block text-gray-700 font-semibold text-left">Email</label>
+              <input
+                type="email"
+                id="email"
+                class="w-full text-gray-700 border bg-white rounded px-3 py-2 mt-1 focus:outline-none focus:ring focus:ring-primary/50"
+                required
+              />
+            </div>
+            <div class="mb-4">
+              <label for="phone" class="block text-gray-700 font-semibold text-left">Phone</label>
+              <input
+                type="phone"
+                id="phone"
+                class="w-full text-gray-700 border bg-white rounded px-3 py-2 mt-1 focus:outline-none focus:ring focus:ring-primary/50"
+                required
+              />
+            </div>
+            <div class="mb-4">
+              <label for="address" class="block text-gray-700 font-semibold text-left">Address</label>
+              <input
+                type="address"
+                id="address"
+                class="w-full text-gray-700 border bg-white rounded px-3 py-2 mt-1 focus:outline-none focus:ring focus:ring-primary/50"
+                required
+              />
+            </div>
+            <div class="mb-4">
+              <label for="aboutMe" class="block text-gray-700 font-semibold text-left">About Me</label>
+              <textarea
+                id="aboutMe"
+                class="w-full text-gray-700 border bg-white rounded px-3 py-2 mt-1 focus:outline-none focus:ring focus:ring-primary/50"
+                rows="4"
+                required
+              ></textarea>
+            </div>
+          </form>
+        </div>
+        
+        <!-- Modal Footer -->
+        <div class="flex justify-end space-x-4">
+          <button @click="closeModal" class="bg-gray-300 hover:bg-gray-400 text-gray-700 px-4 py-2 rounded-md">Close</button>
+          <button @click="submitForm" class="bg-primary hover:bg-primaryHover text-white px-4 py-2 rounded-md">Submit</button>
+        </div>
+      </div>
+    </div>
+</template>
+
+<script>
+export default {
+  props: {
+    showModal: {
+      type: Boolean,
+      required: true,
+    },
+  },
+  methods:{
+    closeModal() {
+        this.$emit('close');
+      },
+    submitForm(){
+        console.log('berhasil')
+    }
+  }
+  // Code lainnya
+};
+</script>
+
+
+<style lang="scss" scoped>
+
+</style>
--- src/components/pages/user/Profile.vue
+++ src/components/pages/user/Profile.vue
@@ -2,7 +2,9 @@
   <div class="max-w-6xl mx-auto p-6">
     <div class="grid grid-cols-1 md:grid-cols-12 gap-6">
       <!-- Profile Card -->
-      <div class="bg-white p-6 rounded-lg shadow-md h-96 md:col-span-4  border-t-4 border-primary">
+      <div
+        class="bg-white p-6 rounded-lg shadow-md h-96 md:col-span-4 border-t-4 border-primary"
+      >
         <div class="text-center">
           <img
             alt="Profile picture of John Doe"
@@ -37,10 +39,11 @@
           </ul>
         </div>
       </div>
-      
 
       <!-- Profile Details -->
-      <div class="bg-white p-6 rounded-lg shadow-md md:col-span-8 border-t-4 border-primary">
+      <div
+        class="bg-white p-6 rounded-lg shadow-md md:col-span-8 border-t-4 border-primary"
+      >
         <div class="grid grid-cols-1 md:grid-cols-2 gap-6">
           <div>
             <h3 class="text-lg font-semibold text-gray-700">Full Name</h3>
@@ -78,13 +81,17 @@
           </div>
         </div>
         <div class="mt-6 text-end">
-            <button
-      @click="showModal = true"
-      class="bg-primary hover:bg-primaryHover text-white px-4 py-2 rounded-md"
-    >
-      Edit Profile
-    </button>
-    
+          <button
+            @click="showModalProfile = true"
+            class="bg-primary hover:bg-primaryHover text-white px-4 py-2 rounded-md"
+          >
+            Add Profile
+          </button>
+          <ModalEditProfile
+            v-if="showModalProfile"
+            :showModal="showModalProfile"
+            @close="showModalProfile = false"
+          />
         </div>
       </div>
     </div>
@@ -99,12 +106,17 @@
             <div class="mt-4"></div>
             <div class="mt-6 text-end">
               <button
+                @click="showModalExperience = true"
                 class="bg-primary hover:bg-primaryHover text-white px-4 py-2 rounded-md"
               >
-                Edit
+                Add Experience
               </button>
+              <ModalAddExperience
+                v-if="showModalExperience"
+                :showModal="showModalExperience"
+                @close="showModalExperience = false"
+              />
             </div>
-            
           </div>
 
           <!-- Education -->
@@ -112,11 +124,17 @@
             <h3 class="text-lg font-semibold text-gray-600">Education</h3>
             <div class="mt-4"></div>
             <div class="mt-6 text-end">
-              <button
+                <button
+                @click="showModalEducation = true"
                 class="bg-primary hover:bg-primaryHover text-white px-4 py-2 rounded-md"
               >
-                Edit
+                Add Education
               </button>
+              <ModalAddEducation
+                v-if="showModalEducation"
+                :showModal="showModalEducation"
+                @close="showModalEducation = false"
+              />
             </div>
           </div>
 
@@ -126,10 +144,16 @@
             <div class="mt-4"></div>
             <div class="mt-6 text-end">
               <button
+                @click="showModalSkill = true"
                 class="bg-primary hover:bg-primaryHover text-white px-4 py-2 rounded-md"
               >
-                Edit
+                Add Skill
               </button>
+              <ModalAddSkill
+                v-if="showModalSkill"
+                :showModal="showModalSkill"
+                @close="showModalSkill = false"
+              />
             </div>
           </div>
 
@@ -139,80 +163,54 @@
             <div class="mt-4"></div>
             <div class="mt-6 text-end">
               <button
+                @click="showModalCertificate = true"
                 class="bg-primary hover:bg-primaryHover text-white px-4 py-2 rounded-md"
               >
-                Edit
+                Add Certificate
               </button>
+              <ModalAddCertificate
+                v-if="showModalCertificate"
+                :showModal="showModalCertificate"
+                @close="showModalCertificate = false"
+              />
             </div>
           </div>
         </div>
       </div>
     </div>
   </div>
-
-
-  <!-- modal edit prfile -->
-    <!-- Modal -->
-    <div v-if="showModal" class="fixed inset-0 bg-gray-900 bg-opacity-50 flex items-center justify-center z-50">
-      <!-- Modal Container -->
-      <div class="bg-white p-6 rounded-lg shadow-lg w-11/12 md:w-1/3">
-        <!-- Modal Header -->
-        <div class="flex justify-between items-center border-b pb-3">
-          <h3 class="text-xl font-semibold text-gray-700">Edit Profile</h3>
-          <button @click="showModal = false" class="text-gray-500 hover:text-gray-700">✕</button>
-        </div>
-        
-        <!-- Modal Body (Form) -->
-        <div class="mt-4">
-          <form @submit.prevent="submitForm">
-            <div class="mb-4">
-              <label for="name" class="block text-gray-700 font-semibold">Name</label>
-              <input
-                type="text"
-                id="name"
-                v-model="formData.name"
-                class="w-full text-gray-700 border bg-white rounded px-3 py-2 mt-1 focus:outline-none focus:ring focus:ring-primary/50"
-                required
-              />
-            </div>
-            <div class="mb-4">
-              <label for="email" class="block text-gray-700 font-semibold">Email</label>
-              <input
-                type="email"
-                id="email"
-                v-model="formData.email"
-                class="w-full text-gray-700 border bg-white rounded px-3 py-2 mt-1 focus:outline-none focus:ring focus:ring-primary/50"
-                required
-              />
-            </div>
-          </form>
-        </div>
-        
-        <!-- Modal Footer -->
-        <div class="mt-6 flex justify-end space-x-4">
-          <button @click="showModal = false" class="bg-gray-300 hover:bg-gray-400 text-gray-700 px-4 py-2 rounded-md">Close</button>
-          <button @click="submitForm" class="bg-primary hover:bg-primaryHover text-white px-4 py-2 rounded-md">Submit</button>
-        </div>
-      </div>
-    </div>
 </template>
 
 <script>
+import ModalAddExperience from "../../User/ModalAddExperience.vue";
+import ModalEditProfile from "../../User/ModalAddProfile.vue";
+import ModalAddEducation from "../../User/ModalAddEducation.vue";
+import ModalAddSkill from "../../User/ModalAddSkill.vue";
+import ModalAddCertificate from "../../User/ModalAddCertificate.vue";
+
+
+
 export default {
   data() {
     return {
-      showModal: false,
-      formData: {
-        name: '',
-        email: '',
-      },
+      showModalExperience: false,
+      showModalProfile: false,
+      showModalEducation: false,
+      showModalSkill: false,
+      showModalCertificate: false,
+
     };
   },
   methods: {
-    submitForm() {
-      alert(`Name: ${this.formData.name}\nEmail: ${this.formData.email}`);
-      this.showModal = false; // Close modal after submitting
-    },
+  },
+  components: {
+    ModalEditProfile,
+    ModalAddExperience,
+    ModalAddEducation,
+    ModalAddSkill,
+    ModalAddCertificate,
+
+
   },
 };
 </script>
