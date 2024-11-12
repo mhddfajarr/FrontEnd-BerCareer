dd16a012e9b8dd8a6ccd2555aa63982a0c51e009
bgwnrakas <begawanraka97@gmail.com>
2024-11-12 08:36:57 +0700

    Dashboard Admin

--- src/components/pages/admin/dashboard.vue
+++ src/components/pages/admin/dashboard.vue
@@ -1,13 +1,32 @@
 <template>
     <div class="content-wrapper">
-    <div class="breadcrumbs text-sm">
+    <div class="breadcrumbs text-sm mb-5">
         <ul>
             <li><a>Dashboard</a></li>
         </ul>
     </div>
 
     <!-- Main content -->
-    
+        <div class="flex justify-between">
+            <div class="card bg-base-100 w-100 shadow-xl border-t-4 border-t-primary">
+                <div class="card-body">
+                    <h2 class="card-title">Total Applicates</h2>
+                    <p>If a dog chews shoes whose shoes does he choose?</p>
+                    <div class="card-actions justify-end">
+                    <button class="btn btn-primary">Buy Now</button>
+                    </div>
+                </div>
+            </div>
+            <div class="card bg-base-100 w-100 shadow-xl border-t-4 border-t-primary">
+                <div class="card-body">
+                    <h2 class="card-title">Total Post Job</h2>
+                    <p>If a dog chews shoes whose shoes does he choose?</p>
+                    <div class="card-actions justify-end">
+                    <button class="btn btn-primary">Buy Now</button>
+                    </div>
+                </div>
+            </div>
+        </div>
     <!-- /.content -->
 </div>
 </template>
